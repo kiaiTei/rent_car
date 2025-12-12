58d997cd6bce2858fca9ff04586dace52c737a96
tachinosuke <tachinosuke@gmail.com>
2025-12-12 14:34:10 +0900

    aaa

--- src/main/java/com/example/demo/controller/FrontController.java
+++ src/main/java/com/example/demo/controller/FrontController.java
@@ -1,47 +1,47 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FrontController {
-	@RequestMapping( "/login" )
+	@PostMapping( "/login" )
 	public String login() {
 		return "login";
 	}
-	@RequestMapping( "/login_employee" )
+	@PostMapping( "/login_employee" )
 		public String login_employee() {
 			return "login_employee";	
      }
-	@RequestMapping( "/reserve" )
+	@PostMapping( "/reserve" )
 	public String reserve() {
 		return "reserve";
      }
-	@RequestMapping( "/reserve_input" )
+	@PostMapping( "/reserve_input" )
 	public String reserve_input() {
 		return "reserve_input";
      }
-	@RequestMapping( "/reserve_update" )
+	@PostMapping( "/reserve_update" )
 	public String reserve_update() {
 		return "reserve_update";
      }
-	@RequestMapping( "/reserve_update_result" )
+	@PostMapping( "/reserve_update_result" )
 	public String reserve_update_result() {
 		return "reserve_update_result";
      }
-	@RequestMapping( "/reserve_delete" )
+	@PostMapping( "/reserve_delete" )
 	public String reserve_delete() {
 		return "reserve_delete";
      }
-	@RequestMapping( "/reserve_delete_result" )
+	@PostMapping( "/reserve_delete_result" )
 	public String reserve_delete_result() {
 		return "reserve_delete_result";
      }
-	@RequestMapping( "/customer" )
+	@PostMapping( "/customer" )
 	public String customer() {
 		return "customer";
      }
-	@RequestMapping( "/employee_exclusive" )
+	@PostMapping( "/employee_exclusive" )
 	public String employee_exclusive() {
 		return "employee_exclusive";
      }
