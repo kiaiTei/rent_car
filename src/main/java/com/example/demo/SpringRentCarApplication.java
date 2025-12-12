fcac08fb6792226a6125d614b716485dcd5bb3dc
xyz <xyz@xyz.jp>
2025-12-12 10:25:03 +0900

    初期の状態をアップします。

---
+++ src/main/java/com/example/demo/SpringRentCarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRentCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRentCarApplication.class, args);
+	}
+
+}
