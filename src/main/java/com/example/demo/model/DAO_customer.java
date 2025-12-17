4b2ba0ef74f67f3fb54910e606ea6b726947f9b4
xyz <xyz@xyz.jp>
2025-12-17 13:00:00 +0900

    customer_update

--- src/main/java/com/example/demo/model/DAO_customer.java
+++ src/main/java/com/example/demo/model/DAO_customer.java
@@ -1,13 +1,16 @@
 package com.example.demo.model;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
-
 @Repository
 public class DAO_customer {
 
@@ -34,8 +37,9 @@ public class DAO_customer {
 	    String phone = (String) m.get("phone");
 	    String email = (String) m.get("email");
 	    String address = (String) m.get("address");
+	    String password = (String) m.get("password");
 
-	    return new Entitycostmer(id, name, phone, email, address);
+	    return new Entitycostmer(id, name, phone, email, address,password);
 	}
 
 	
@@ -54,27 +58,46 @@ public class DAO_customer {
             String phone = (String) m.get("phone"); 
             String email = (String) m.get("email");
             String address = (String) m.get("address");
+            String password = (String) m.get("password");
 
-            Entitycostmer c = new Entitycostmer(id, name, phone, email, address);
+            Entitycostmer c = new Entitycostmer(id, name, phone, email, address,password);
             list.add(c);
         }
         return list;
     }
 
     // 追加
+//    public void insert(Entitycostmer customer) {
+//
+//        String sql = """
+//            INSERT INTO customer_info
+//            (name, phone, email, address, password)
+//            VALUES (?, ?, ?, ?,?)
+//            """;
+//
+//        db.update(sql,
+//                customer.getName(),
+//                customer.getPhone(),
+//                customer.getEmail(),
+//                customer.getAddress(),
+//                customer.getPassword());
+//    }
+    
     public void insert(Entitycostmer customer) {
+        String sql = "INSERT INTO customer_info(name, phone, email, address, password) VALUES(?,?,?,?,?)";
 
-        String sql = """
-            INSERT INTO customer_info
-            (name, phone, email, address)
-            VALUES (?, ?, ?, ?)
-            """;
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        db.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, customer.getName());
+            ps.setString(2, customer.getPhone());
+            ps.setString(3, customer.getEmail());
+            ps.setString(4, customer.getAddress());
+            ps.setString(5, customer.getPassword());
+            return ps;
+        }, keyHolder);
 
-        db.update(sql,
-                customer.getName(),
-                customer.getPhone(),
-                customer.getEmail(),
-                customer.getAddress());
+        customer.setCustomerId(keyHolder.getKey().intValue());
     }
 
 
@@ -83,26 +106,59 @@ public class DAO_customer {
         String sql = "DELETE FROM customer_info WHERE customer_id = ?";
         db.update(sql, customerId);
     }
+    
+    
+    
+    public Entitycostmer getCusById(int id) {
+	      String sql = "SELECT * FROM customer_info WHERE customer_id = ?";
+	      System.out.println(id);
+	      Map<String, Object> m = db.queryForMap(sql, id);
+
+	      return new Entitycostmer(
+	          (int) m.get("customer_id"),
+	          (String) m.get("name"),
+	          (String) m.get("phone"),
+	          (String) m.get("email"),
+	          (String) m.get("address"),
+	          (String) m.get("password")
+	      );
+	  }
+
+    
+    
  // 更新
-    public void update(Entitycostmer customer) {
-
-        String sql = """
-            UPDATE customer_info
-            SET name = ?,
-                phone = ?,
-                email = ?,
-                address = ?
-            WHERE customer_id = ?
-            """;
-
-        db.update(sql,
-                customer.getName(),
-                customer.getPhone(),
-                customer.getEmail(),
-                customer.getAddress(),
-                customer.getCustomerId());
+//    public void update(Entitycostmer customer) {
+//
+//        String sql = """
+//            UPDATE customer_info
+//            SET name = ?,
+//                phone = ?,
+//                email = ?,
+//                address = ?,
+//                password = ?
+//            WHERE customer_id = ?
+//            """;
+//
+//        db.update(sql,
+//                customer.getName(),
+//                customer.getPhone(),
+//                customer.getEmail(),
+//                customer.getAddress(),
+//                customer.getPassword(),
+//                customer.getCustomerId());
+//    }
+    
+    public void update(int id, String name, String phone, String email, String address, String password) {
+	      String sql = "UPDATE customer_info SET name = ?,phone = ?,email = ?,address = ?,password = ? WHERE customer_id = ?";
+	      db.update(sql, name, phone, email, address, password, id);
+	  }
+    
+    public int cus_deleteById(int id) {
+        String sql = "DELETE FROM customer_info WHERE customer_id = ?";
+        return db.update(sql, id);
     }
 
+	
 
 
 }
