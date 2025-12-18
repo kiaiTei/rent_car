92d93622fa068402cba11e7752787345a08d8c87
Matsumoto Tomonari <abcd@abcd.jp>
2025-12-18 10:12:08 +0900

    4

--- src/main/java/com/example/demo/controller/FrontController.java
+++ src/main/java/com/example/demo/controller/FrontController.java
@@ -492,13 +492,6 @@ public class FrontController {
     public String reserve_delete_success() {
         return "reserve_delete_success";
     }
-<<<<<<< HEAD
-
-
-
-=======
-    
->>>>>>> branch 'master' of https://github.com/kiaiTei/rent_car.git
 }
 
     
